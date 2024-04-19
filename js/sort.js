03dce97496975397727b8f7adaa18672ce6bcf2d
Nishchith H D <nishchithhd2002@gmail.com>
2024-04-19 10:18:59 +0530

    new project

--- js/sort.js
+++ js/sort.js
@@ -1,10 +1,12 @@
-let x=["pineapple","mango","orange","apple"]
+// let x=["pineapple","mango","orange","apple"]
+
+let x=[1,5,2]
 let y=''
 for(i=0;i<=x.length;i++)
 {
     for(j=i;j<=x.length;j++)
     {
-        if(x[i]>x[j]){
+        if(x[i]<x[j]){
         y=x[i]
         x[i]=x[j]
         x[j]=y
